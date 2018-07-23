663f8e83a8ccef89cdc3d7794d939e46f2c1b084
bingo3000 <t352134833@126.com>
2018-07-23 21:37:05 +0800

    add log config

---
+++ chapter4-3/src/main/java/com/bingo/chapter43/controller/LogController.java
@@ -0,0 +1,25 @@
+package com.bingo.chapter43.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+
+    Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @RequestMapping("/log")
+    public String addLog() {
+        //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
+
+        return "logs";
+    }
+
+}
